1557787e0ede29719f1c922f89fae0d8008d0cf0
Fabiano Muscardi <fabiano.junior@araujo.com.br>
2023-01-31 09:49:06 -0300

    A few settings

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,9 +9,6 @@ public class ChessMatch {
 	private Board board;
 
 	public ChessMatch() {
-	}
-
-	public ChessMatch(Board board) {
 		board = new Board(8, 8);
 		initialSetup();
 	}
@@ -19,7 +16,6 @@ public class ChessMatch {
 	public ChessPiece[][] getPieces() {
 
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-
 		for (int i = 0; i < board.getRows(); i++) {
 			for (int j = 0; j < board.getColumns(); j++) {
 				mat[i][j] = (ChessPiece) board.piece(i, j);
